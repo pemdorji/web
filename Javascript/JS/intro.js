eb036942042966d0138d084aab85e43481d73f84
pemdorji <05240142.jnec@rub.edu.bt>
2025-09-16 15:04:20 +0600

    Add a new files for javascript

---
+++ Javascript/JS/intro.js
@@ -0,0 +1,12 @@
+function changecolor(){
+    document.getElementById("div3").innerHTML = "<h1>green</h1>";
+}
+function replaceEverything(){
+    document.write ("everything was replace")
+}
+function writeIntoTheLog(){
+    console.log("debugging")
+}
+function displayAlert(){
+    window.alert("This is an alert")
+}
